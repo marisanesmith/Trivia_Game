1ca81a521da117fa35845140b55be8862afb6f91
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-01-08 14:47:55 -0500

    updated CSS, HTML & JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,8 @@
-var startBtn = document.getElementById("startBtn")
-var questionsEl = document.getElementById("questions")
+const startBtn = document.getElementById("startBtn")
 
+const questionsEl = document.getElementById("questions")
 
-var startGame = document.querySelector(".container");
+const startGame = document.querySelector(".container");
 console.log(startGame)
 
 var timer = document.getElementById("timer");
@@ -31,22 +31,41 @@ var questions = [
     }
 ]
 
+startBtn.addEventListener('click', startGame)
 
-
-function beginGame() {
+function startGame() {
     startGame.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
+    startBtn.classList.add('hide')
     // add timer & start the clock
     timer // initiate timer and countdown
-    retrieveQuestions();
+    getNextQuestions();
 }
 
-function retrieveQuestions() {
+var count = 30;
+var counter = setInterval (timer, 1000)
+
+function countDown() {
+    count = count -1;
+    if (count <= 0) {
+        clearInterval(counter);
+        //counter method, do something here
+        return;
+    }
+}
+
+document.getElementById("timer").innerHTML = count + "seconds left";
+
+function getNextQuestion() {
     //this function will get all the questions from the array of objects
     //create for loop and buttons for the answer choices
+}
+
+function selectAnswer {
+
 }
 
 //separate function for on click when user clicks right or wrong question. Subtract from timer: if/else statements
 
 //separate function for addEventListener "click"
-startBtn.onclick = beginGame;
+// startBtn.onclick = startGame;
